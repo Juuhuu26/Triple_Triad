ceddd7735980dbf82ea71b9df8d9fcbda6a89e72
Juergen <juhuber@htwg-konstanz.de>
2016-04-06 16:35:33 +0200

    first steps

---
+++ htwg.se.tripletriad/src/de/htwg/tripletriad/model/Card.java
@@ -0,0 +1,57 @@
+package de.htwg.tripletriad.model;
+
+public class Card {
+	
+	private String name;
+	private int up, down, left, right;
+	private char color;
+	
+	public Card(String name, int up, int down, int left, int right, char color) {
+		
+		this.name = name;
+		this.up = up;
+		this.down = down;
+		this.left = left;
+		this.right = right;
+		this.color = color;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public int getUp() {
+		return this.up;
+	}
+	
+	public int getDown() {
+		return this.down;
+	}
+	
+	public int getLeft() {
+		return this.left;
+	}
+	
+	public int getRight() {
+		return this.right;
+	}
+	
+	public char getColor() {
+		return this.color;
+	}
+
+	public void changeColor() {
+		
+		if(this.color == 'r')
+			this.color = 'b';
+		else
+			this.color = 'r';
+	}
+	
+	@Override
+	public String toString(){
+		
+		return this.name + ": " + this.up + ", " + this.down + ", "
+				+ this.left + ", " + this.right;
+	}
+}
---
+++ htwg.se.tripletriad/test/de/htwg/tripletriad/model/CardTest.java
@@ -0,0 +1,57 @@
+package de.htwg.tripletriad.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CardTest {
+	
+	private Card card;
+	
+	@Before
+	public void setCard() throws Exception {
+		card = new Card("Test", 1, 1, 1, 1, 'b');
+	}
+	
+	@Test
+	public void testGetName() {
+		assertEquals("Test", card.getName());
+	}
+	
+	@Test
+	public void testGetUp() {
+		assertEquals(1, card.getUp());
+	}
+	
+	@Test
+	public void testGetDown() {
+		assertEquals(1, card.getDown());
+	}
+	
+	@Test
+	public void testGetLeft() {
+		assertEquals(1, card.getLeft());
+	}
+	
+	@Test
+	public void testGetRight() {
+        assertEquals(1, card.getRight());
+	}
+	
+	@Test
+	public void testChangeColor() {
+		card.changeColor();
+		assertEquals('r', card.getColor());
+		card.changeColor();
+		assertEquals('b', card.getColor());
+	}
+	
+	@Test
+	public void testToString() {
+		String result = card.toString();
+		System.out.println(result);
+		String expected = "{";
+		assertEquals(expected.substring(1, 1),result.subSequence(1, 1));
+	}
+}
