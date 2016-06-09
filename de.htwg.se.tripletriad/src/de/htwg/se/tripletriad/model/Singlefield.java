f5b857be6756dc9853ce5ec0a5c0c7bcb9685ddf
Juuhuu26 <juuhuu26@gmail.com>
2016-06-09 21:58:39 +0200

    method added

--- de.htwg.se.tripletriad/src/de/htwg/se/tripletriad/model/Singlefield.java
+++ de.htwg.se.tripletriad/src/de/htwg/se/tripletriad/model/Singlefield.java
@@ -1,19 +1,22 @@
 package de.htwg.se.tripletriad.model;
 
 public class Singlefield {
-	
-	private Card card;
-	
-	public Singlefield() {
-		this.card = null;
-	}
-	
-	public void setCard(Card card) {
-		this.card = card;
-	}
-	
-	public Card getCard() {
-		return this.card;
-	}
-	
+
+    private Card card;
+
+    public Singlefield() {
+       this.card = null;
+    }
+
+    public void setCard(Card card) {
+        this.card = card;
+    }
+
+    public Card getCard() {
+        return this.card;
+    }
+
+    public boolean isBusy(){
+        return card != null;
+    }
 }
